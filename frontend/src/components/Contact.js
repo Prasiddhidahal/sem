b3728b3cd7a5958d59f1dc4a27fa106088b3f681
Prasiddhi Dahal <87261517+Prasiddhidahal@users.noreply.github.com>
2023-11-10 23:33:33 +0545

    done with contact page for now. will be working on webcam frontend

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,30 +1,58 @@
-// Import any necessary modules or libraries
-// ...
+// Import necessary modules or libraries
+import React from "react";
+import "./Contact.css"; // Import the CSS file
+import facebook from "../Images/facebook.png";
+import instagram from "../Images/insta.png";
+import twitter from "../Images/twitter.png";
+import contact from "../Images/contact.png";
+import Navbar from "./Navbar";
 
-// Define a function to handle form submission
 function handleSubmit(event) {
   event.preventDefault();
-  // Get form data and do something with it
-  // ...
 }
 
-// Define the contact form component
 function ContactForm() {
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" required />
+    <div>
+      <Navbar />;
+      <div className="contact-container">
+        <img src={contact} alt="contact" className="contactimg" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
 
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
+          <button type="submit">Submit</button>
+        </form>
 
-      <label htmlFor="message">Message:</label>
-      <textarea id="message" name="message" required></textarea>
-
-      <button type="submit">Submit</button>
-    </form>
+        <div className="contact-social">
+          <p>Or contact us through:</p>
+          <div className="social">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebook} alt="facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={instagram} alt="insta" />
+            </a>
+            <a
+              href="https://www.Twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} alt="twitter" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-// Export the contact form component
 export default ContactForm;
