84365dc9c8c97f86829c7cf2b04ff8383d794e50
Prasiddhi Dahal <87261517+Prasiddhidahal@users.noreply.github.com>
2023-11-13 22:29:20 +0545

    Done frontend

---
+++ frontend/src/components/Edit.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Edit.css"; // Import your CSS file
+import Layout from "./Layout";
+
+const Edit = () => {
+  return (
+    <Layout>
+      <div className="edit-container">
+        <h2>Edit Account</h2>
+        <form className="edit-form">
+          <div className="form-group">
+            <label>Name:</label>
+            <input type="text" placeholder="John Doe" />
+          </div>
+
+          <div className="form-group">
+            <label>Description:</label>
+            <textarea placeholder="Web Developer | Passionate about coding"></textarea>
+          </div>
+
+          <div className="form-group">
+            <label>Age:</label>
+            <input type="text" placeholder="25" />
+          </div>
+
+          <div className="form-group">
+            <label>Location:</label>
+            <input type="text" placeholder="City, Country" />
+          </div>
+
+          <div className="form-group">
+            <label>Number:</label>
+            <input type="text" placeholder="+1234567890" />
+          </div>
+
+          <div className="form-group">
+            <button type="button">
+              <Link to="/Profile">Save Changes</Link>
+            </button>
+          </div>
+        </form>
+
+        <div className="edit-options">
+          <Link to="/Profile">Cancel</Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Edit;
