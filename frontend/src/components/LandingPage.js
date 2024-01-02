1877f06cc4f34fce84ad7a442315f4efa5bbd84f
Dilasha Upadhyay <dilashaupa@gmail.com>
2024-01-02 23:31:38 +0545

    UI changes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,32 +2,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import "./LandingPage.css";
-import loveImage from "../Images/Land2.png";
-//import facebook from "../Images/facebook.png";
-//import instagram from "../Images/insta.png";
-//import twitter from "../Images/twitter.png";
+//import loveImage from "../Images/Land2.png";
+import landi from "../Images/landi.png";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <div className="container">
+        <img src={landi} alt="landi" className="landi" />
         <h1>Gesture Sync</h1>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap');
-        </style>
         <h2>Translating Hands to Heart</h2>
 
-        <div className="images"></div>
-        <img src={loveImage} alt="Love" className="love" />
         <p>Know What Your Loved One Is Trying to Tell You</p>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Kenia&family=Libre+Baskerville:wght@700&display=swap');
-        </style>
       </div>
-      {}
       <Footer />
     </div>
   );
